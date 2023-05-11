5aca5fc3641fb2f57cb2b525b7e597e723f6f130
Sech <sech@itu.dk>
2023-05-11 12:02:20 +0200

    New code to create CSV files, but unfortunately doesnt seem to work. I believe that the reason is, that kaagle doesnt allow for direct zip url link, so it does not properly download. Needs kaagle api to work.

--- e_commerce_data.py
+++ e_commerce_data.py
@@ -2,8 +2,11 @@ import os
 import requests, zipfile, io
 import sqlite3
 import pandas as pd
+import numpy as np
 import seqlog
 import logging
+from io import BytesIO
+
 
 # Create connection to seqlog and logger_name
 seqlog.log_to_seq(
@@ -16,23 +19,26 @@ seqlog.log_to_seq(
 
 logger = logging.getLogger(__name__)
 
-# Create path for data file, load and create data file if not exist
-if not os.path.exists('../Data'):
-    os.mkdir('../Data')
-
-# Download data if it is unavailable and store as zip file
-if 'e_com_behaviour_data.zip' not in os.listdir('../Data'):
-    logger.info('Dataset loading.')
-    url = r"https://www.kaggle.com/datasets/mkechinov/ecommerce-behavior-data-from-multi-category-store/download" \
+# merge the two CSV files from url and create binary file to use for SQL database - seems that this is not possible since kaagle doesnt allow for direct zip link
+url = "https://www.kaggle.com/datasets/mkechinov/ecommerce-behavior-data-from-multi-category-store/download" \
           "?datasetVersionNumber=8"
-    output = r'../Data/e_com_behaviour_data.zip'
-    r = requests.get(url, allow_redirects=True)
-    with open(output, 'wb') as f:
-        f.write(r.content)
-    logger.info('Loaded.')
 
-data_path = "../Data/e_com_behaviour_data.zip"
+r = requests.get(url)
+buf1 = BytesIO(r.content)
+df_files = []
+with zipfile.ZipFile(buf1, 'r') as f:
+    for name in f.namelist():
+        if name.endswith('.csv'):
+            with f.open(name) as zd:
+                dataframe = pd.read_csv(zd)
+                df_files.append(dataframe)
+
+df = pd.concat(df_files)
+np.asarray(df.values).tofile('data_binary.dat')
+
+
 
 # Create SQL database if not exist, extract the data from the path and load it to the database
 con = sqlite3.connect('ecom_database.db')
-zipfile.ZipFile.extractall(data_path)
+
+
